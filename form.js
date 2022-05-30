af6f6c05121dcfe3c0829e423cecf072485a976a
Esther <arikeadeoluwaseun@yahoo.com>
2022-05-30 01:51:23 -0700

    initial

--- form.js
+++ form.js
@@ -1,34 +1,19 @@
-   const constraints = {
-       name: {
-           presence: { allowEmpty: false }
-       },
-       email: {
-           presence: { allowEmpty: false },
-           email: true
-       },
-       message: {
-           presence: { allowEmpty: false }
-       }
-   };
 
-   const form = document.getElementById('contact-form');
+   function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
-   form.addEventListener('submit', function (event) {
-     const formValues = {
-         name: form.elements.name.value,
-         email: form.elements.email.value,
-         message: form.elements.message.value
-     };
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
 
-     const errors = validate(formValues, constraints);
-
-     if (errors) {
-       event.preventDefault();
-       const errorMessage = Object
-           .values(errors)
-           .map(function (fieldValues) { return fieldValues.join(', ')})
-           .join("\n");
-
-       alert(errorMessage);
-     }
-   }, false);
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
